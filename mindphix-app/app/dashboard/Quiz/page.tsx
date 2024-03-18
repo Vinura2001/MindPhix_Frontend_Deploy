4318d3118733764b0c546ec7911e3f5bf036f682
Wanninayaka Mudiyanselaage Vinura Imesh <w.m.v.imesh02@gmail.com>
2024-03-18 17:43:40 +0530

    fix error 2

--- mindphix-app/app/dashboard/Quiz/page.tsx
+++ mindphix-app/app/dashboard/Quiz/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, useEffect } from 'react';
 import BaseLayout from '../BaseLayout';
 import { ref, push, update } from 'firebase/database';
 import { database } from "@/app/firebase/config";
@@ -58,7 +58,12 @@ const QuizPage = () => {
     q9: null,
   });
   const [result, setResult] = useState('');
-  const [currentWeek, setCurrentWeek] = useState(getStoredWeek());
+  const [currentWeek, setCurrentWeek] = useState(1); // Default to week 1
+
+  useEffect(() => {
+    const storedWeek = getStoredWeek();
+    setCurrentWeek(storedWeek);
+  }, []);
 
   const handleAnswerChange = (e: ChangeEvent<HTMLSelectElement>, questionNumber: number) => {
     setAnswers((prevAnswers) => ({
